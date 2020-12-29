4eb49f8f1d0714c9daf84e1247904479743b766c
patrick-rush <patrick.a.rush@gmail.com>
2020-12-28 20:02:43 -0500

    add user login controller actions

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -24,5 +24,29 @@ class Users::SessionsController < Devise::SessionsController
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  # end 
+
+  private
+
+  def respond_with(resource, _opts = {})
+    render json: {
+    status: {code: 200, message: 'Logged in successfully.'},
+    data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+    }
+  end
+  
+  def respond_to_on_destroy
+    if current_user 
+      render json: {
+        status: 200,
+        message: "logged out successfully"
+      }, status: :ok
+    else
+      render json: { 
+        status: 401,
+        message: "Couldn't find an active session."
+      }, status: :unauthorized
+    end
+  end
+  
 end
