5185a72b346bc95668650ebac98bce90d76a13b3
patrick-rush <patrick.a.rush@gmail.com>
2020-12-30 19:06:17 -0500

    add seeds for user and plants

--- Gemfile
+++ Gemfile
@@ -32,6 +32,9 @@ gem 'fast_jsonapi'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'faker'
+  gem 'database_cleaner'
+  gem 'dotenv-rails'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'database_cleaner'
+
+DatabaseCleaner.clean_with(:truncation)
+
+# Seed Users
+1.times do
+    user = User.new
+    user.email = Faker::Internet.email
+    user.password = ENV['PASSWORD']
+    user.save
+end
+
+# Seed Plants
+5.times do 
+    plant = Plant.new
+    species = ['ficus', 'spider plant', 'succulent', 'cactus', 'pathos']
+    locations = ['living room', 'kitchen', 'bedroom', 'bathroom', 'dining room']
+    random_species_index = Faker::Number.within(range: 0..4)
+    random_location_index = Faker::Number.within(range: 0..4)
+    random_watering_frequency = Faker::Number.within(range: 1..30)
+    random_fertilizing_frequency = Faker::Number.within(range: 1..12)
+    plant.name = Faker::Name.first_name
+    plant.species = species[random_species_index]
+    plant.location = locations[random_location_index]
+    plant.watering_frequency = "#{random_watering_frequency} days"
+    plant.fertilizing_frequency = "#{random_fertilizing_frequency} months"
+    plant.user_id = 1
+    plant.save
+end
+
