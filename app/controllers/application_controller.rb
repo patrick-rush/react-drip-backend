494e077178f6aa31514bd981360dfa21092d20c5
patrick-rush <patrick.a.rush@gmail.com>
2021-05-30 17:31:59 -0400

    reconfigure for jwt blacklist strategy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
-    def current_user
-        User.first
-    end
+    before_action :authenticate_user!
+    # def current_user
+    #     User.first
+    # end
 end
